5e49a1982652c94ecd5381e59a723517a70bf714
deepak mohan <krishna66827@outlook.com>
2017-05-25 00:57:53 +0530

    commiting partial

--- Samples/Playlists/cs/ReturnScenario/ReturnScenario.xaml.cs
+++ Samples/Playlists/cs/ReturnScenario/ReturnScenario.xaml.cs
@@ -16,18 +16,48 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 using SDKTemplate;
+using SDKTemp.ViewModel;
+
 namespace SDKTemp
 {
+    public delegate void OrderListChangedDelegate(OrderListCC sender);
     public sealed partial class OrderListCC : Page
     {
         public static OrderListCC Current;
+        private OrderViewModel _lastSelectedItem;
+        public List<OrderViewModel> orderList;
+        public event OrderListChangedDelegate OrderListChangedEvent;
         public OrderListCC()
         {
-            this.InitializeComponent();
             Current = this;
+            this.InitializeComponent();         
             if (CustomerASBCC.Current == null)
                 throw new Exception("CustomerASBCC should be loaded before orderListCC");
             CustomerASBCC.Current.SelectedCustomerChangedEvent += new SelectedCustomerChangedDelegate(UpdateMasterListViewItemSource);
+        }
+        private void UpdateMasterListViewItemSource(CustomerViewModel customer = null)
+        {
+            if (customer == null)
+                Current.orderList = OrderDataSource.Orders;
+            else
+            {
+                Current.orderList = OrderDataSource.RetrieveOrdersByMobileNumber(customer.MobileNo);
+            }
+            OrderListCC.Current.OrderListChangedEvent?.Invoke(OrderListCC.Current);
+            MasterListView.ItemsSource = orderList;
+        }
+        private void MasterListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var clickedItem = (OrderViewModel)e.ClickedItem;
+            _lastSelectedItem = clickedItem;
+            // Play a refresh animation when the user switches detail items.
+            EnableContentTransitions();
+        }
+        private void EnableContentTransitions()
+        {
+            DetailContentPresenter.ContentTransitions.Clear();
+            // just for adding the transition on the content selection.
+            //DetailContentPresenter.ContentTransitions.Add(new EntranceThemeTransition());
         }
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
@@ -61,9 +91,8 @@ namespace SDKTemp
         private void LayoutRoot_Loaded(object sender, RoutedEventArgs e)
         {
             // Assure we are displaying the correct item. This is necessary in certain adaptive cases.
-            MasterListView.SelectedItem = _lastSelectedItem;
+            //MasterListView.SelectedItem = _lastSelectedItem;
         }
-
         private void DisableContentTransitions()
         {
             if (DetailContentPresenter != null)
