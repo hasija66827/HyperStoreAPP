8190155aec7d8b6d09a3b963436207019e75d423
deepak mohan <krishna66827@outlook.com>
2017-09-09 11:57:21 +0530

    adding quantity purchased attribute in price quoted by supplier

--- Samples/Playlists/cs/Models/PriceQuotedBySupplier.cs
+++ Samples/Playlists/cs/Models/PriceQuotedBySupplier.cs
@@ -11,6 +11,7 @@ namespace Models
         public Guid SupplierId { get; set; }
         public DateTime OrderDate { get; set; }
         public decimal PurchasePrice { get; set; }
+        public decimal QuantityPurchased { get; set; }
         public TSupplier Supplier { get; set; }
     }
 }
